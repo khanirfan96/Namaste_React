6966df255b9fe25df415b8ae6f397826dc3477e9
khanirfan96 <irfank.ik141@gmail.com>
2023-12-04 16:40:38 +0530

    first push

--- App.js
+++ App.js
@@ -33,11 +33,10 @@
  * </div>
  * </div>
  *
- * ReactElement(Object) => HTML(Browser Understands) 
+ * ReactElement(Object) => HTML(Browser Understands)
  */
 
 // This Approach to create a sibling element from React Code upper example 3
-
 const parent_sibling1 = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "I am a h1 tag"),
@@ -47,10 +46,9 @@ const parent_sibling1 = React.createElement("div", { id: "parent" }, [
     React.createElement("h1", {}, "I am a h1 tag"),
     React.createElement("h2", {}, "I am a h2 tag"),
   ]),
-]);
+]); 
 
 // This Approach to create a sibling element from React Code upper example 2
-
 const parent_sibling2 = React.createElement(
   "div",
   { id: "parent" },
@@ -61,7 +59,6 @@ const parent_sibling2 = React.createElement(
 );
 
 // This Approach to create a nested element from React Code upper example 1
-
 const parent_child = React.createElement(
   "div",
   { id: "parent" },
@@ -73,7 +70,6 @@ const parent_child = React.createElement(
 );
 
 // This Approach to create an element from React Code
-
 const heading = React.createElement(
   "h1",
   { id: "heading", xyz: "abcdfff" }, //this is called attribute
@@ -86,3 +82,4 @@ const heading = React.createElement(
 console.log(parent_sibling1);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent_sibling1);
+ 
